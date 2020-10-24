d26481b99d3457ec97f01a04b13e5ca64b63ba43
kritika.dutt.upadhyay kritika.dutt.upadhyay <kdutt1@uncc.edu>
2020-10-23 23:01:47 -0400

    jwt changes

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/api/login', (req, res) => {
         
     for(let user of users){
         if(username == user.username && password == user.password){
-            let token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '15seconds'});
+            let token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '3minutes'});
             res.json({
                 success:true,
                 err: null,
